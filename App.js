772ef21e570d95fd1ba4072db11bb56c9ee39029
Alejandro Hernandez <alejandroalamina@hotmail.com>
2020-07-02 15:26:54 -0500

    added: styled components and test of styles

--- App.js
+++ App.js
@@ -1,114 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+import styled from 'styled-components';
+const CustomButton = props => (
+	<ButtonContainer
+		onPress={() => alert('Hi!')}
+		backgroundColor={props.backgroundColor}
+	>
+		<ButtonText textColor={props.textColor}>{props.text}</ButtonText>
+	</ButtonContainer>
+);
+const ButtonContainer = styled.TouchableOpacity`
+	width: 100px;
+	height: 100px;
+	padding: 12px;
+	border-radius: 50px;
+	background-color: ${props => props.backgroundColor};
+`;
+const ButtonText = styled.Text`
+	font-size: 15px;
+	color: ${props => props.textColor};
+	text-align: center;
+`;
+const Container = styled.View`
+	flex: 1;
+	background-color: papayawhip;
+	justify-content: center;
+	align-items: center;
+`;
+const Title = styled.Text`
+	font-size: 24px;
+	font-weight: 500;
+	color: palevioletred;
+`;
+export default class App extends React.Component {
+	render() {
+		return (
+			<Container>
+				<CustomButton text="Me" textColor="#01d1e5" backgroundColor="lavenderblush" />
+			</Container>
+		);
+	}
+}
