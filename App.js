de6320d8b07cb2a4582ed81217e22e96b6d81a07
Cesar Gomez <cesar@yellowme.mx>
2020-07-10 11:11:46 -0500

    added: Log list and database

--- App.js
+++ App.js
@@ -1,42 +1,45 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { Component, useState } from 'react';
+import { Text, View, TextInput, ScrollView, FlatList, Button } from 'react-native';
 import styled, { ThemeConsumer, ThemeProvider } from 'styled-components';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
 import Log from "./components/Log";
 import theme from './theme';
-import { Button } from 'react-native';
+import LogList from './components/LogList';
 
 const Container = styled.View`
 	flex: 1;
-	background-color: ${ props => props.theme.FOURTH };
+	background-color: ${ props => props.theme.FOURTH};
 	justify-content: center;
 	align-items: center;
 `;
+
 const Stack = createStackNavigator();
 
-const dummyLogs = ({ navigation }) => (
-	<Container>
-		<Button title="go editor" onPress={() => navigation.navigate('Editor')}></Button>
-	</Container>
-);
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		return (
-			<NavigationContainer>
-				<ThemeProvider theme={ theme }>
-				<Stack.Navigator initialRouteName="Logs" >
-					<Stack.Screen name="Logs" component={dummyLogs} />
-					<Stack.Screen name="Editor">
-						{ props => <Log {...props} theme={ theme } date=" 27 June 2020" />}
-					</Stack.Screen>
-				</Stack.Navigator>
-				</ThemeProvider>
-			</NavigationContainer>
-		);
-	}
+  render() {
+    return (
+      <NavigationContainer>
+        <ThemeProvider theme={theme}>
+          <Stack.Navigator initialRouteName="Logs" >
+            <Stack.Screen
+              name="Logs"
+              component={LogList}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Editor"
+              component={Log}>
+            </Stack.Screen>
+          </Stack.Navigator>
+        </ThemeProvider>
+      </NavigationContainer>
+    );
+  }
 }
